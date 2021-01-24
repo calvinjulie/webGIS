7b74a7cba5632424a010da8b8c6136b774936a58
calvinjulie <72082198+calvinjulie@users.noreply.github.com>
2021-01-23 19:20:14 -0500

    Update index.js

--- introductory/index.js
+++ introductory/index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 43.6532, lng: 79.3832 },
+    center: { lat: 43.6532, lng: -79.3832 },
     zoom: 8,
   });
 }
