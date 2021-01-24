ab95c1b7c56548c9db56823753446ee3b06a1c59
calvinjulie <72082198+calvinjulie@users.noreply.github.com>
2021-01-23 19:23:26 -0500

    Update index.js

--- introductory/index.js
+++ introductory/index.js
@@ -1,5 +1,6 @@
 let map;
 
+// Found city of Toronto and changed zoom to 10 for the "city" level
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 43.6532, lng: -79.3832 },
